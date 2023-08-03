309cdd411ec46268313a5d037e6f674ca4030a8d
Sanghyeok Kang <31328994+skang188144@users.noreply.github.com>
2023-08-03 15:27:18 -0400

    Add initial project source code
    Added initial source code before version control was implemented.
    Components and features added include:
    * src/main/java/com/kangsk/detox/MainActivity.java: Removed action bar,
    stretched DecorView past system window elements, attached the three
    fragments, and set an OnItemSelectedListener for all three fragment
    options in the bottom navigation bar.
    * src/main/java/com/kangsk/detox/HomeFragment.java: Added onCreateView()
    method to inflate the fragment's layout view. The same applies for
    LockdownFragment.java and SettingsFragment.java.
    * src/main/res/layout/activity_main.xml: Added the main activity's
    layout resource, with the root element being ConstraintView, and its
    child elements being a FragmentContainerView and a BottomNavigationView.
    * src/main/res/layout/fragment_home.xml: Added default code for the
    fragment layout resource. Root element is a FrameLayout, with various
    widgets as its children. Same applies for fragment_lockdown.xml and
    fragment_settings.xml.
    * src/main/res/drawable: Added drawable versions of icon images, in addition
    to vector images of the bottom navigation bar elements.
    * src/main/res/mipmap-anydpi-v26: Added two versions of mipmap icon images,
    a version with a label, and one without a label.
    * src/main/res/values/themes.xml: Added various tags to format the bottom
    navigation bar.

---
+++ app/src/main/java/com/kangsk/detox/MainActivity.java
@@ -0,0 +1,75 @@
+package com.kangsk.detox;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
+
+    // private non-static fields
+    private BottomNavigationView mBottomNav;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*
+         * remove top action bar:
+         * makes the decorview to stretch throughout the entire system window, ignoring system widgets
+         */
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
+        // bottom navigation bar
+        mBottomNav = findViewById(R.id.bottom_nav);
+        mBottomNav.setOnItemSelectedListener(this);
+
+        // attach home fragment to activity, plus some other fragment configuration changes
+        getSupportFragmentManager()
+                .beginTransaction()
+                .setReorderingAllowed(true)
+                .addToBackStack(null)
+                .add(R.id.fragment_container, HomeFragment.class, null)
+                .commit();
+    }
+
+    // bottom navigation bar item selection listener
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        // home item selected
+        if (id == R.id.menu_home) {
+            replaceFragment(HomeFragment.class);
+            return true;
+        // lockdown item selected
+        } else if (id == R.id.menu_lockdown) {
+            replaceFragment(LockdownFragment.class);
+            return true;
+        // settings item selected
+        } else if (id == R.id.menu_settings) {
+            replaceFragment(SettingsFragment.class);
+            return true;
+        }
+
+        return false;
+    }
+
+    // fragment transition/replace helper method
+    public void replaceFragment(Class<? extends Fragment> newFragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .setReorderingAllowed(true)
+                .addToBackStack(null)
+                .replace(R.id.fragment_container, newFragment, null)
+                .commit();
+    }
+
+}
